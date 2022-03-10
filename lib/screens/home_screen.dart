97156d5a16fd0fd2c677ba662bc7b25a2655eb95
Ignacio Irigoitia <ignacio.irigoitia@gmail.com>
2022-03-10 16:10:47 -0300

    obtener cordenadas terminado

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,8 +1,9 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:location/location.dart';
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -12,18 +13,35 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  static const CameraPosition puntoInicial = CameraPosition(
-    target: LatLng(37.42796133580664, -122.085749655962),
-    zoom: 14.4746,
-  );
+  
 
   final Completer<GoogleMapController> _controller = Completer();
 
+
+  @override
+  void initState() {
+    super.initState();
+    calcularLatLng("Argentina, chaco");
+  }
+  double? lat;
+  double? lng;  
+
+  Future<void> calcularLatLng(String ciudad) async {
+    List<Location> locations = await locationFromAddress(ciudad);
+    lat = locations[0].latitude;
+    lng = locations[0].longitude;
+    setState((){});
+  }
+
   @override
   Widget build(BuildContext context) {
+
+    final size = MediaQuery.of(context).size;
+
+
     // Marcadores
     Set<Marker> markers = Set<Marker>();
-    markers.add(Marker(
+    markers.add (const Marker(
       markerId: MarkerId('my-location'),
       position: LatLng(0.0, 0.0),
     ));
@@ -32,13 +50,21 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text('Mapa'),
       ),
-      body: GoogleMap(
-        markers: markers,
-        mapType: MapType.normal,
-        initialCameraPosition: puntoInicial,
-        onMapCreated: (GoogleMapController controller) {
-          _controller.complete(controller);
-        },
+      body:  ListView(
+        children: [
+          if(lng != null && lat != null)
+            SizedBox(
+              height: size.height - 70,
+              child: GoogleMap(
+                markers: markers,
+                mapType: MapType.normal,
+                initialCameraPosition: CameraPosition(target: LatLng(lat!, lng!), zoom: 14),
+                onMapCreated: (GoogleMapController controller) {
+                  _controller.complete(controller);
+                },
+              ),
+            ),
+        ],
       ),
     );
   }
